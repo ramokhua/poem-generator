74460b408bd7c3bcdff5a5c6b804832457a2c9e6
Boitsholo Ramokhua <ramokhuaboitsholo@gmail.com>
2025-02-10 15:08:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,5 +9,22 @@ function generatePoem(event) {
   });
 }
 
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "e6cc64taaa3ed8def581fc3fao0d0b1c";
+  let context =
+    "You are poet. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate an English poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
+}
+
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
